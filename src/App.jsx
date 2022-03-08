d6ed1dd05c634850ba52415929ff953fc8a4215a
Emerson Asto <emerar.mct@gmail.com>
2022-03-08 10:46:18 -0500

    instaled tailwind

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-xl font-bold text-primary-1 font-roboto'>
+      App
+    </div>
+  )
+}
+
+export default App
