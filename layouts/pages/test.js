de96684a11461ed4c721f80f722b0ccccc8841ab
Emerson Asto <emerar.mct@gmail.com>
2022-03-08 10:15:21 -0500

    Created react template and reorganize files and folders in repository

--- layouts/pages/test.js
+++
@@ -1,9 +0,0 @@
-const boton = document.querySelector('#boton');
-const menu = document.querySelector('#right');
-const navbar = document.querySelector('#navbar');
-
-boton.addEventListener('click', () => {
-  menu.classList.toggle('hidden')
-  navbar.classList.toggle('h-64')
-  navbar.classList.toggle('h-20')
-})
