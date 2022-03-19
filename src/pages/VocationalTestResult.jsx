8e5c28065ee729baae5196706e4c647c253cc8f8
carlos <carlosjuniorrvega@hotmail.com>
2022-03-19 13:50:45 -0500

    add vocational test result reactjs

---
+++ src/pages/VocationalTestResult.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import HeaderLogin from '../components/molecules/headerLogin/HeaderLogin'
+
+import NavBar from '../components/organisms/navBar/NavBar'
+import Footer from '../components/organisms/footer/Footer'
+
+const VocationalTestResult = () => {
+  return (
+    <section>
+      <NavBar/>
+      <Footer/>
+    </section>
+  )
+}
+
+export default VocationalTestResult
