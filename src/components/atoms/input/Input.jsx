de707a06da1764dc834000f17432e74a23ff2505
unknown <p.barbosa93@gmail.com>
2022-03-21 20:59:01 -0500

    feature: added University profile form page

--- src/components/atoms/input/Input.jsx
+++ src/components/atoms/input/Input.jsx
@@ -4,8 +4,8 @@ import PropTypes from 'prop-types';
 const Input = ({ type, name }) => {
   return (
     <div className='relative z-0 mb-6 w-full group'>
-      <input type={type} name={name} id={name} className='block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer' placeholder='' required />
-      <label htmlFor={name} className='absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'>First name</label>
+      <input type={type} name={`floating_${name}`} id={`floating_${name}`} className='block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-teal-500 focus:outline-none focus:ring-0 focus:border-teal-500 peer' placeholder='' required />
+      <label htmlFor={`floating_${name}`} className='absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-teal-500 peer-focus:dark:text-teal-400 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'>{name}</label>
     </div>
   );
 };
--- src/components/molecules/universityForm/UniversityForm.jsx
+++ src/components/molecules/universityForm/UniversityForm.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
+import Button from '../../atoms/button/Button';
 import Input from '../../atoms/input/Input';
 
 const UniversityForm = () => {
   return (
-    <form action=''>
+    <form action='' className=' md:grid grid-cols-3 gap-8'>
       <Input type='text' name='University name' />
-      <Input type='text' name='mission' />
-      <Input type='text' name='vision' />
-      <Input type='text' name='How to apply' />
+      <Input type='text' name='Mission' />
+      <Input type='text' name='Vision' />
       <Input type='text' name='Country' />
       <Input type='text' name='Department' />
       <Input type='text' name='City' />
+      <Input type='text' name='How to apply' />
       <Input type='text' name='Other important information' />
+      <Button text='Academic offer' />
+      <Button text='Save' />
     </form>
   );
 };
