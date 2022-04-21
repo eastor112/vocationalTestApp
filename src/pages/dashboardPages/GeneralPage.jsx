34c0d8442d162d87b396916365b64a9243b401ce
Emerson Asto <emerar.mct@gmail.com>
2022-04-20 22:20:14 -0500

    created test result in api

--- src/pages/dashboardPages/GeneralPage.jsx
+++ src/pages/dashboardPages/GeneralPage.jsx
@@ -1,7 +1,33 @@
-import { Link, useOutletContext } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { Link, useNavigate, useOutletContext } from 'react-router-dom';
+import Swal from 'sweetalert2';
+import { createTestResult } from '../../services/vocationalServices';
 
 const GeneralPage = () => {
   const width = useOutletContext();
+  const navigate = useNavigate();
+
+  const { user } = useSelector((state) => state.auth);
+
+  const handleCreateTestResult = () => {
+    Swal.fire({
+      title: 'Initializing the test...',
+      html: 'Wait a moment...',
+      allowEscapeKey: false,
+      allowOutsideClick: false,
+      didOpen: () => {
+        Swal.showLoading();
+      },
+    });
+    createTestResult(user.uid)
+      .then((testResult) => {
+        console.log(testResult);
+        Swal.close();
+      });
+
+    // navigate('/dashboard/test');
+  };
+
   return (
     <main className={`min-h-screen w-screen pt-4 pr-10 bg-light-1 ${width === 64 ? 'pl-72' : 'pl-24'}`}>
 
@@ -28,9 +54,13 @@ const GeneralPage = () => {
               to a range of affined careers.
             </p>
 
-            <Link to='/dashboard/test' className='text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-md text-sm px-5 py-2.5 text-center mr-2 mb-2'>
+            <button
+              type='button'
+              className='text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-md text-sm px-5 py-2.5 text-center mr-2 mb-2'
+              onClick={handleCreateTestResult}
+            >
               Go to the test
-            </Link>
+            </button>
 
           </div>
         </div>
